0de135c451320b2f94da9254db623aadf37b994c
MiKL5 <miklgaillard@gmail.com>
2022-12-09 09:17:02 +0100

    moving files

--- 002 Les bases de React/014 les props/react-app/src/Item.js
+++
@@ -1,9 +0,0 @@
-function Item (props){
-
-    console.log(props);
-
-    return <h1>{props.number}</h1>
-
-}
-
-export default Item;
--- 007 React-router/044 le router et le switch/src/Components/Projets.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Projets() {
-    return (
-        <h1>Section  PROJETS</h1>
-    )
-}
