6b2ccc1b791d5d8382e3b05f454033f237f06fb9
MiKL <miklgaillard@gmail.com>
2022-12-26 20:24:15 +0100

    Probs

--- bases_react/04_webapp/react-app/src/App.js
+++ bases_react/04_webapp/react-app/src/App.js
@@ -1,23 +1,24 @@
 // Le state c'est les donn'es d'un composant.
 import {useState} from 'react'
+import Item from './Item'
 
 function App() {
   // console.log(useState);
 
   console.log("MISE À JOUR !"); // Des que le state d'un composant change, le state est réexécuté
 
-  const [monState, setMonState] = useState(10)
+  const [monState, setMonState] = useState(10);
   // console.log(MonState);
   // console.log(setMonState);
-  const modifyState=() => {
+  const modifyState = () => {
     setMonState(20);
   }
  // Les évênements en lower camel case
   return (
   <div className="app">
     <h1>Hello state : {monState}</h1>
-    <Item />
     <button onCLick={modifyState}>Change state</button>
+    <Item />
   </div>
   );
 }
