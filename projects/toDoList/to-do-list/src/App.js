144df75f940603774b48cb7096faab20b8dccfe3
MiKL <miklgaillard@gmail.com>
2022-12-28 16:55:55 +0100

    Changemnts mineurs

--- projects/toDoList/to-do-list/src/App.js
+++ projects/toDoList/to-do-list/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Form from './components/Form'
+import bg from "./images/yanivCohen.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" /*style={{ backgroundImage:`url(${bg})`,backgroundRepeat:"no-repeat" }} */ >
       <h1 className="text-center mt-3">To do list</h1>
       <Form />
     </div>
