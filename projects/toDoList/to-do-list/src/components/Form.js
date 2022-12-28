39187ab1b2a89f128ee0e3d162152f505c6f5ecc
MiKL <miklgaillard@gmail.com>
2022-12-28 13:55:10 +0100

    Affichier les éléments

--- projects/toDoList/to-do-list/src/components/Form.js
+++ projects/toDoList/to-do-list/src/components/Form.js
@@ -1,8 +1,14 @@
 import {useState} from 'react'
 import Item from './Item'
 
-export default function form(){
+export default function Form(){
 
+    const [dataArr, setDataArr] = useState([
+        {txt: "Promener le chien"},
+        {txt: "Sport"},
+        {txt: "Coder avec React"},
+    ])
+    
     return(
         // Le contenu visuel
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
@@ -12,13 +18,16 @@ export default function form(){
                 <button className="mt-2 btn btn-primary d-block">Envoyer</button>
             </form>
 
-            <h2>Liste des tâhehs</h2>
+            <h2>Liste des tâches</h2>
             <ul className="list-group">
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
+                {dataArr.map((item,index) => {
+                    return( <Item
+                        txt={item.txt}
+                        key={index}
+                        // id={index}
+                        // del={delete}
+                        /> )
+                })}
             </ul>
         </div>
     )
