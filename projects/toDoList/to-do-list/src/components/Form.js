57fca85b588637bee1a8c79f4f8e6a7cce7afdf2
MiKL <miklgaillard@gmail.com>
2022-12-28 15:30:12 +0100

    Ajouter les éléments

--- projects/toDoList/to-do-list/src/components/Form.js
+++ projects/toDoList/to-do-list/src/components/Form.js
@@ -1,15 +1,17 @@
-import {useState} from 'react'
+import {useState} from 'react' // le state est utilisable autant que c'est nécessaire
 import Item from './Item'
 import {v4 as uuidv4} from 'uuid'
 
 export default function Form(){
 
     const [dataArr, setDataArr] = useState([
-        {txt: "Promener le chien", id: uuidv4()},
-        {txt: "Sport", id: uuidv4()},
-        {txt: "Coder avec React", id: uuidv4()},
+        // {txt: "Promener le chien", id: uuidv4()},
+        // {txt: "Sport", id: uuidv4()},
+        // {txt: "Coder avec React", id: uuidv4()},
     ])
     // console.log(uuidv4())
+
+    const [stateInput, setStateInput] = useState();
 
     const deleteElement = id =>{
         console.log(id);
@@ -20,12 +22,35 @@ export default function Form(){
         setDataArr(filteredState)
     }// L'immutabilité (avec React) dataArr n'est jamais changer directement. Un nouveau tableau est retourné. Et ça évite à React de chercher à savoir ce qui a été mis à jour.
 
+    // le bouton ajoute l'élément
+    const addTodo = e =>{
+        e.preventDefault(); // prévient le comportement pad défaut d'actualiser la page. Donc évite que la page se mette à jour à chaque envoi du formulaire
+        // nv tableau
+        const newArr = [...dataArr] // srtt si c'est un tableau ou un objet retour d'un nv state, avec une copie du state, plus ce qui va changer
+        const newTodo = {}; // nv objet
+        newTodo.txt = stateInput;
+        newTodo.id = uuidv4();
+        newArr.push(newTodo); // envoi dans dataArr
+        setDataArr(newArr); // change le state avec des données fraiches dans le tableau
+        setStateInput(''); // la case se vide après envoi
+    }
+
+    const linkedInput = e => {
+        console.log(e);
+        setStateInput(e);
+    }
+
     return(
         // Le contenu visuel
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-            <form className='mb-3'>
+            <form onSubmit={e => addTodo(e)} className='mb-3'>
                 <label htmlFor="todo" className="form-label mt-3">Tâche à faire</label>
-                <input type="text" className="form-control" id="todo" />
+                <input 
+                value={stateInput}
+                onChange ={e => linkedInput(e.target.value) }
+                type="text" 
+                className="form-control" 
+                id="todo" />
                 <button className="mt-2 btn btn-primary d-block">Envoyer</button>
             </form>
 
