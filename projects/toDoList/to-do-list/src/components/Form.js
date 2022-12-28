0e7873e5804f1715b35c0a08a43067fef9ad00b5
MiKL <miklgaillard@gmail.com>
2022-12-28 14:26:50 +0100

    Supprimer les éléments

--- projects/toDoList/to-do-list/src/components/Form.js
+++ projects/toDoList/to-do-list/src/components/Form.js
@@ -1,14 +1,25 @@
 import {useState} from 'react'
 import Item from './Item'
+import {v4 as uuidv4} from 'uuid'
 
 export default function Form(){
 
     const [dataArr, setDataArr] = useState([
-        {txt: "Promener le chien"},
-        {txt: "Sport"},
-        {txt: "Coder avec React"},
+        {txt: "Promener le chien", id: uuidv4()},
+        {txt: "Sport", id: uuidv4()},
+        {txt: "Coder avec React", id: uuidv4()},
     ])
-    
+    // console.log(uuidv4())
+
+    const deleteElement = id =>{
+        console.log(id);
+        // filter les élément aprés la suppression en ne gardant que ceux qui ne sont pas supprimés
+        const filteredState = dataArr.filter(item => {
+            return item.id !== id;
+        })
+        setDataArr(filteredState)
+    }// L'immutabilité (avec React) dataArr n'est jamais changer directement. Un nouveau tableau est retourné. Et ça évite à React de chercher à savoir ce qui a été mis à jour.
+
     return(
         // Le contenu visuel
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
@@ -20,12 +31,12 @@ export default function Form(){
 
             <h2>Liste des tâches</h2>
             <ul className="list-group">
-                {dataArr.map((item,index) => {
+                {dataArr.map((item) => {
                     return( <Item
                         txt={item.txt}
-                        key={index}
-                        // id={index}
-                        // del={delete}
+                        key={item.id}
+                        id={item.id}
+                        delFunc={deleteElement}
                         /> )
                 })}
             </ul>
--- projects/toDoList/to-do-list/src/components/Item.js
+++ projects/toDoList/to-do-list/src/components/Item.js
@@ -2,7 +2,8 @@ export default function Item(props) {
     return (
         <li className="border d-flex justify-content-between align-items-center p-2 m-2">
             <div className="p-3">{props.txt}</div>
-            <bouton className="btn btn-danger p-2 h-50">Supprimer</bouton>
+            <bouton className="btn btn-danger p-2 h-50" onClick={ () => props.delFunc(props.id) }>Supprimer</bouton>
         </li>
     )
 }
+// () =>  est une fonction anonyme pour ne pas exécuter tout de suite la méthode
