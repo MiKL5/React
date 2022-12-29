b3a624e50991de74a8922c5973216159e069f54c
MiKL <miklgaillard@gmail.com>
2022-12-29 21:19:09 +0100

    Séléctionner un tableau d'élément

--- React_detail/react-advanced/src/App.js
+++ React_detail/react-advanced/src/App.js
@@ -6,17 +6,15 @@ export default function App() {
   const [toggle, setToggle] = useState(true);
 
   // retour d'un objet de ce que j'ai sélectionner en valeur de première propriété qui s'appel current
-  const ref = useRef();
-  console.log(ref); // ici c'est undéfini car rien n'a envore été afficher
+  const ref = useRef([]);
 
   //toujours useEffect à la fin du 1er affichage pour rappeler
   useEffect(() => {
-    console.log(ref); // contient la vidéo
-    console.log(ref.current); // contient les détails de la vidéo
+    console.log(ref); // il y a bien un tableau de 3 vidéos
     // mettre en pause aprés 1,5 seonde avec la fonction js pause()
-    setTimeout(() => {
-      ref.current.pause();
-    }, 1500);
+//  setTimeout(() => {
+//      ref.current.pause();
+//    }, 1500);
   })
 
   // le bouton supprime le timer
@@ -24,17 +22,30 @@ export default function App() {
     setToggle(!toggle)
   }
 
+  // ref oassera ci-dessous
+  const addToRef = el => { //el pour élément
+    console.log(el);
+    // vérif si el est tjrs là
+    if(el && !ref.current.includes(el)){ // est-ce que le tableau inclu l'élément ?
+      ref.current.push(el); //si ce'est faux' le mettre au tableau
+    }
+  }
 
   return (
     <div className="App">
-      <video ref={ref}  width="750" height="500" autoPlay controls muted>
+      <video ref={addToRef}  width="750" height="500" autoPlay controls muted>
         <source src={Video} type='video/mp4'/>
       </video>
+
+      <video ref={addToRef}  width="750" height="500" autoPlay controls muted>
+        <source src={Video} type='video/mp4'/>
+      </video>
+
+      <video ref={addToRef}  width="750" height="500" autoPlay controls muted>
+        <source src={Video} type='video/mp4'/>
+      </video>
+
       <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
 }
-// autoplay fonctionne queés'il n'y a pas de son
-// useRef pour use référence
-// ref est lié dans video ligne 25
-// useRef est rarement utiliser, il est srtt utile pour les animations, on peut faire un site entier sans s'en servir
