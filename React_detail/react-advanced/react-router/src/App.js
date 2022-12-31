723f061820d5f15ffa8de4baac5d0cbad1b35a46
MiKL <miklgaillard@gmail.com>
2022-12-31 22:37:44 +0100

    Les bases de 'React-router'

---
+++ React_detail/react-advanced/react-router/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Home from './components/Home/Home';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
