dca2e6c864b96df5e160c7cec5531f84c88d517f
MiKL <miklgaillard@gmail.com>
2022-12-31 18:56:55 +0100

    Navbar responsive

--- React_detail/react-advanced/src/App.js
+++ React_detail/react-advanced/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 // import Modal from './Components/Modal/Modal';
 import Accord from './Components/Accordion/Accord'
+import Navbar from './Components/Navbar/Navbar';
 
 export default function App() {
   return (
     <>
+      <Navbar />
       {/* <Modal /> */}
       <Accord />
     </>
