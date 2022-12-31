2657c6ab2db16fd3d8181ca5e2f6c24e0e61be4f
MiKL <miklgaillard@gmail.com>
2022-12-31 01:39:52 +0100

    'React.memo' vs 'useMemo'

--- React_detail/react-advanced/src/App.js
+++ React_detail/react-advanced/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useMemo} from 'react'
 import './App.css'
 import Content from './Content'
 
@@ -15,23 +15,20 @@ export default function App() {
     // setToggle(toggle) // avec React on ne peut pas modifier le state directment, en anglaiś on peut voir la phrase "NEVER MUTATE STATE DIRECTLY" donc il faut recopier les valeurs pour modifier.
     setToggle(newArr);
   };
-  // vérif dans la console que ça fonctionne
-  console.log("mise à jour");
+  // useMemo c'est pour les valeur de référence, il va avoir une fonction callback qui va retourner le tableau
+  const tableau = useMemo(() => {
+    return [1,2,3,4]
+  }, []) // il ne se recréer plus à chaque affichage du parent
 
   return (
     <div className="App">
-      <Content>
-        <h1>Titre de l'article 1</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia dolores pariatur fugiat accusantium veniam iure perferendis, quae officiis quis voluptatum exercitationem repellendus maiores alias tempore dignissimos minima, laudantium expedita?</p>
-      </Content><Content>
-        <h1>Titre de l'article 2</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia dolores pariatur fugiat accusantium veniam iure perferendis, quae officiis quis voluptatum exercitationem repellendus maiores alias tempore dignissimos minima, laudantium expedita?</p>
-      </Content><Content>
-        <h1>Titre de l'article 3</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia dolores pariatur fugiat accusantium veniam iure perferendis, quae officiis quis voluptatum exercitationem repellendus maiores alias tempore dignissimos minima, laudantium expedita?</p>
-      </Content>
+      <Content num={tableau} />
       <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
 }
 // une constante empêche la réassignation d'une aleur primitive, en plus de React qui ne le permet pas directement
+// useMemo (mémorisation) permet d'améliorer les performances d'une application React
+// 'props.children' est mis à jour à chaque mise à jour de son parent même s'il ne change pas et pour palier, la mémorisation entre en jeu
+// useMemo fonctionne avec tous les autres props SAUF 'props.children'
+// 'React.memo' pour les valeurs primitive et 'useMemo' pour les valeurs de référence
--- React_detail/react-advanced/src/Content.js
+++ React_detail/react-advanced/src/Content.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function content(props) {
-    console.log(props); // il y a dans la console un objett children contenant un tableau avec les h1, p et leurs contenus
+function Content(props) {
+    console.log("Mise à jour"); // il y a dans la console un objett children contenant un tableau avec les h1, p et leurs contenus
     // aficher el contenu à l'écran cf dans la div
     return (
         <div className="content">
-            {props.children}
+            {props.num}
         </div>
     )
 }
+export default React.memo(Content); // export default descend afin de wraper le composant avec le méthode React 'memo'
+// fonctionne comme ça sauf pour un tableau, c'est une valeur de référence alors qu'un nombre est une valeur primitive, c'est là que 'useMemo' entre en jeu
 // Les props.children sont utiles quand on ne sait pas ce qu'on va recevoir, mais qu'on veut l'avoir à un endroit prédis.
