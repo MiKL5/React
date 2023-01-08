2ca4cc9e3d12c76c1eb38f2842ecebae58062bf7
MiKL <miklgaillard@gmail.com>
2023-01-08 21:36:34 +0100

    Establishment

---
+++ React_detail/react-context/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Content from './Component/Content/Content';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
