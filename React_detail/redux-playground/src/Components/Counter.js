26a1008c6fb957b83de75233f12cc0364319b7fa
MiKL <miklgaillard@gmail.com>
2023-01-14 21:38:50 +0100

    Créer un 'Reducer'

--- React_detail/redux-playground/src/Components/Counter.js
+++ React_detail/redux-playground/src/Components/Counter.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux'; // ce hook de React-redux permet l'accés aux données
+import { useSelector, useDispatch } from 'react-redux'; // ce hook de React-redux permet l'accés aux données
 
 export default function Counter() {
   const count = useSelector(state => state.count) // avec cette fonction call back ce hook permet de se connecter directement au store pour utiliser directemt le state
+  const dispatch = useDispatch(); // cette méthode retuourn la fonction dispatch pour envoyer un action
+  const incrFunc = () => { // action d'incrémentation
+    dispatch({
+      type: "INCR"
+    })
+  }
+  const decrFunc = () => { // décrémentaiton
+    dispatch({
+      type: "DECR"
+    })
+  }
   return (
       <div>
-          <h1>Les données : {count}</h1>        
+          <h1>Les données : {count}</h1>
+          <button onClick={decrFunc}>-1</button>
+          <button onClick={incrFunc}>+1</button>
       </div>
   )
 }
--- React_detail/redux-playground/src/Reducers/CounterReducer.js
+++ React_detail/redux-playground/src/Reducers/CounterReducer.js
@@ -4,7 +4,22 @@ const INITIAL_STATE ={
 
 // state = initial_state et prend des actions sur le state
 export default function CounterReducer(state = INITIAL_STATE, action){
-
+    switch(action.type){
+        // INCREMENTER
+        case 'INCR': {
+          return{ // le state n'est JAMAIS DIRECTEMENT MODIFIER DONC,
+            ...state, // le spread operator copie ce qui est dans le state est sera forcément prise en compte, donc passera au dessus de l'autre
+            count : state.count + 1 // ajoute 1 à la copie
+            }
+        }
+        // DECREMENTER
+        case 'DECR': {
+            return{
+            ...state,
+            count : state.count - 1
+            }
+        }
+    }
     //
     console.log(state);
     return state;
