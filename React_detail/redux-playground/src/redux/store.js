248951dc73ceec373edf7eb87ebb70d0f47234c3
MiKL <miklgaillard@gmail.com>
2023-01-14 23:47:19 +0100

    Middleware

--- React_detail/redux-playground/src/redux/store.js
+++ React_detail/redux-playground/src/redux/store.js
@@ -1,6 +1,6 @@
 // le dossier redux est en minuscule pour être différencier des autres
 // le store n'est pas vraiement un composant
-import { createStore, combineReducers } from "redux"; // combineReducers est une méthode pour combiner les reducers
+import { createStore, combineReducers, applyMiddleware } from "redux"; // combineReducers est une méthode pour combiner les reducers
 import CounterReducer from "./Reducers/CounterReducer";
 import AddCartReducer from "./Reducers/addCartReducer";
 
@@ -9,8 +9,20 @@ const rootReducer = combineReducers({
     CounterReducer,
     AddCartReducer
 })
+// ce qui suit est de la purification, c'est à dirr plusiers fonction se retournent à la suite, ça sert à avec à toutes les fonction dans le corps de fonciton
+const customMiddleware = store => next => action => {
+    console.log(store); // donne le state avant que le dispatch ne soit effectué
+    console.log(next); //  est une fonction appelée quand le travail est fini dans le middleware
+    console.log(action); // c'est l'action du dispatch
 
-const store = createStore(rootReducer);
+    const actionModif ={
+        type: "ADDCART",
+        payload: 789
+    }
+    next(actionModif) // donne une nouvelle action
+}
+
+const store = createStore(rootReducer, applyMiddleware(customMiddleware));
 
 export default store;
-// au lieu mettre tout les reducers dans un dossier Reducers on peux créer dans Redux des dossier nominatifs comme articles pannier srtt quand il y a bcp de reducers pour gérer la logique plus intuitivement
+// au lieu mettre tout les reducers dans un dossier Reducers on peux créer dans Redux des dossier nominatifs comme articles panier srtt quand il y a bcp de reducers pour gérer la logique plus intuitivement
