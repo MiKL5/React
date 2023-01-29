cd405bc7d08221be949eb88f8044df13ae747da5
MiKL <miklgaillard@gmail.com>
2023-01-29 19:39:39 +0100

    Card

--- projects/blog/src/App.js
+++ projects/blog/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Containers/Home/Home';
 
 export default function App() {
   return (
     <div className="App">
-
+        <Navbar />
+        <Home />
     </div>
   );
 }
