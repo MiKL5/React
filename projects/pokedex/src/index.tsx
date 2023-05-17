1da91d3d67b6214704cbfdbf9b9c268e63eb45b0
MiKL <miklgaillard@gmail.com>
2023-05-17 21:55:45 +0200

    wip

---
+++ projects/pokedex/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+
+ReactDOM.render( // renvoie ce qui passe par App dans root
+    <App />,
+    document.getElementById('root')
+);
